290277c9be0536a497b6af8b1f5aa3f41474ad37
CodeCraftsman89 <appleid2505@yandex.ru>
2025-02-27 21:16:58 +0300

    save uploaded image

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, html, F
 from aiogram.filters.command import Command
 
-from env import TOKEN # Ваш Telegram token
+from env import TOKEN, INPUT_IMG# Ваш Telegram token
 
 # Включаем логирование
 logging.basicConfig(level=logging.INFO)
@@ -15,7 +15,19 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer(
+        f"Добро пожальвать в конвертер, {html.bold(html.quote(message.from_user.full_name))}! Что вы хотите сделать?",
+        parse_mode=html
+    )
+
+# Сохранение отправленного фото
+@dp.message(F.photo)
+async def download_img(message: types.Message, bot: Bot):
+    await bot.download(
+        message.photo[-1],
+        destination=f"{INPUT_IMG}/{message.photo[-1].file_id}.jpg"
+    )
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
