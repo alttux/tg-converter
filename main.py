b51f68a34620740bde3346cac3a4ae4c84df266e
CodeCraftsman89 <appleid2505@yandex.ru>
2025-03-01 11:57:00 +0300

    update gitignore

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import asyncio
 import logging
+from PIL import Image
+from io import BytesIO
 from aiogram import Bot, Dispatcher, types, html, F
 from aiogram.filters.command import Command
+from aiogram.types import InputFile, Message
 
 from env import TOKEN, INPUT_IMG# Ваш Telegram token
 
@@ -14,21 +17,20 @@ dp = Dispatcher()
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
-async def cmd_start(message: types.Message):
+async def cmd_start(message: Message):
     await message.answer(
         f"Добро пожальвать в конвертер, {html.bold(html.quote(message.from_user.full_name))}! Что вы хотите сделать?",
         parse_mode=html
     )
 
-# Сохранение отправленного фото
 @dp.message(F.photo)
-async def download_img(message: types.Message, bot: Bot):
+async def download_photo(message: Message, bot: Bot):
+    img_name = f"{message.photo[-1].file_id}"
     await bot.download(
         message.photo[-1],
-        destination=f"{INPUT_IMG}/{message.photo[-1].file_id}.jpg"
+        destination=f"{INPUT_IMG}/{img_name}.png"
     )
-
-
+    
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
